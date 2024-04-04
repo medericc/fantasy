12630fb0a3671e37193e35053b5edcef24de82e3
Mede <dalemale09@gmail.com>
2024-04-04 04:17:59 +0200

    week

--- src/Entity/Player.php
+++ src/Entity/Player.php
@@ -13,8 +13,68 @@ class Player
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $forename = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $rate = null;
+
+    #[ORM\ManyToOne(inversedBy: 'players')]
+    private ?team $team = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getForename(): ?string
+    {
+        return $this->forename;
+    }
+
+    public function setForename(?string $forename): static
+    {
+        $this->forename = $forename;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRate(): ?float
+    {
+        return $this->rate;
+    }
+
+    public function setRate(?float $rate): static
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+
+    public function getTeam(): ?team
+    {
+        return $this->team;
+    }
+
+    public function setTeam(?team $team): static
+    {
+        $this->team = $team;
+
+        return $this;
     }
 }
--- src/Entity/Team.php
+++ src/Entity/Team.php
@@ -22,6 +22,14 @@ class Team
     #[ORM\JoinColumn(nullable: false)]
     private ?league $league = null;
 
+    #[ORM\OneToMany(targetEntity: Player::class, mappedBy: 'team')]
+    private Collection $players;
+
+    public function __construct()
+    {
+        $this->players = new ArrayCollection();
+    }
+
    
 
   
@@ -58,6 +66,36 @@ class Team
     public function setLeague(?league $league): static
     {
         $this->league = $league;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Player>
+     */
+    public function getPlayers(): Collection
+    {
+        return $this->players;
+    }
+
+    public function addPlayer(Player $player): static
+    {
+        if (!$this->players->contains($player)) {
+            $this->players->add($player);
+            $player->setTeam($this);
+        }
+
+        return $this;
+    }
+
+    public function removePlayer(Player $player): static
+    {
+        if ($this->players->removeElement($player)) {
+            // set the owning side to null (unless already changed)
+            if ($player->getTeam() === $this) {
+                $player->setTeam(null);
+            }
+        }
 
         return $this;
     }
