0b46ab54e76bed8ee4fc4f9ef6384dc769072999
Mede <dalemale09@gmail.com>
2024-04-20 01:45:57 +0200

    ee

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,6 @@
 import { registerReactControllerComponents } from '@symfony/ux-react';
+
+
 /*
 * Welcome to your app's main JavaScript file!
 *
@@ -14,8 +16,8 @@ const $ = require('jquery');
 require('bootstrap');
 
 // or you can include specific pieces
-// require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
+//require('bootstrap/js/dist/tooltip');
+require('bootstrap/js/dist/popover');
 
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
