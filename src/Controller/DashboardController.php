9d22e45df7b126b6de167a9bb2dbd0f7c10ff608
Mede <dalemale09@gmail.com>
2024-08-09 11:36:08 +0200

    deck show mais plus blocage

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Choice;
+use App\Repository\ChoiceRepository;
 use App\Repository\PlayerRepository;
 use App\Repository\WeekRepository;
 use App\Service\WeekService;
@@ -15,7 +17,7 @@ class DashboardController extends AbstractController
     public function __construct(private readonly WeekService $weekService) {}
 
     #[Route('/dashboard/week/{id}', name: 'app_dashboard_id', methods: ['GET'])]
-    public function index(int $id, PlayerRepository $playerRepository, WeekRepository $weekRepository): Response
+    public function index(int $id, PlayerRepository $playerRepository, WeekRepository $weekRepository, ChoiceRepository $choiceRepository): Response
     {
         $matchesLFB = json_decode(file_get_contents('../matchlfb.json'), true);
         $matchesLF2 = json_decode(file_get_contents('../matchlf2.json'), true);
@@ -34,15 +36,19 @@ class DashboardController extends AbstractController
             throw $this->createNotFoundException('Week not found');
         }
 
-        // Get the selected players for the week
-        $selectedPlayers = $playerRepository->findBy(['selected' => true, 'week' => $week]);
+        // Get the selected players for the specific week from the Choice table
+        $choices = $choiceRepository->findBy(['week' => $week]);
+        $selectedPlayers = [];
+        foreach ($choices as $choice) {
+            $selectedPlayers[] = $choice->getPlayer();
+        }
 
         return $this->render('dashboard/index.html.twig', [
             'weekId' => $id, // Pass the weekId to the template
             'week' => $week, // Ensure the 'week' variable is passed to the template
             'matchesLF2' => $matchesLF2Filtered,
             'matchesLFB' => $matchesLFBFiltered,
-            'selectedPlayers' => $selectedPlayers
+            'selectedPlayers' => $selectedPlayers // Pass selected players to the template
         ]);
     }
 
